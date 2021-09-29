7a94b8326e90134e5145b252f87b4728d4af3014
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2021-09-29 16:50:59 +0300

    Business Dropdown component.

--- js/components/BusinessDropdown.vue.js
+++ js/components/BusinessDropdown.vue.js
@@ -1,5 +1,42 @@
+import useFormatting from '../composables/useFormatting';
+
 export default {
   template: `
-    <h1>This is the Business Dropdown</h1>
+    <div v-if="categories" class="business-list-dropdown form-floating">
+      <select
+        id="business-list-filter"
+        class="form-select"
+        aria-label="Floating label select example"
+        @change="onFilter($event)"
+      >
+        <option value="">All categories</option>
+        <option
+          v-for="category in categories"
+          :key="category.param"
+          :value="category.param"
+        >
+          {{ toTitle(category.title) }}
+        </option>
+      </select>
+      <label for="business-list-filter">Choose a category</label>
+    </div>
   `,
+
+  props: {
+    filterTerm: { type: String, default: '' },
+  },
+  emits: ['update:filterTerm'],
+
+  setup(props, { emit }) {
+    const categories = Vue.inject('categories');
+
+    // Get formatting tools.
+    const { toTitle } = useFormatting();
+
+    const onFilter = (e) => {
+      emit('update:filterTerm', e.target.value);
+    };
+
+    return { categories, toTitle, onFilter };
+  },
 };
