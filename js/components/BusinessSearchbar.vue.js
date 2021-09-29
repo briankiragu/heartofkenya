4c8f449482fc10623c6178cb41e9df301f693ede
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2021-09-29 17:13:47 +0300

    Business Searchbar component.

--- js/components/BusinessSearchbar.vue.js
+++ js/components/BusinessSearchbar.vue.js
@@ -2,4 +2,19 @@ export default {
   template: `
     <h1>This is the Business Searchbar</h1>
   `,
+
+  props: {
+    modelValue: { type: String, default: '' },
+    onSubmit: { type: Function, default: () => { } },
+  },
+
+  emits: ['update:modelValue'],
+
+  setup(props, { emit }) {
+    const onSearch = lodash.debounce((e) => {
+      emit('update:modelValue', e.target.value);
+    }, 275);
+
+    return { onSearch };
+  },
 };
