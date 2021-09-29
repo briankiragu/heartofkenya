eaeb285d795943831eeb5c9bedb7d5b2bbd99f4c
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2021-09-29 17:19:55 +0300

    useFormatting composable.

--- js/composables/useFormatting.js
+++ js/composables/useFormatting.js
@@ -1,3 +1,15 @@
 export default () => {
-  return {}
+  /**
+   * Convert a word/phrase to 'Title Case'.
+   *
+   * @param {string} value
+   * @returns {string}
+   *
+   * @author Brian Kariuki Kiragu <bkariuki@hotmail.com>
+   */
+  const toTitle = (value = '') => value.split('-')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+
+  return { toTitle };
 };
