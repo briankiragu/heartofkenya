67392c8cb579c561791339800972064ab2447c44
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2021-09-29 16:59:26 +0300

    Business Filter Item Component.

--- js/components/BusinessFilterItem.vue.js
+++ js/components/BusinessFilterItem.vue.js
@@ -1,5 +1,32 @@
 export default {
   template: `
-    <h1>This is the Business Filter Item</h1>
+    <div
+      class="business-filter-item"
+      :class="{ active }"
+      @click.prevent="onSelect(item.param)"
+    >
+      <span v-show="active" class="business-filter-item__icon">
+        <!-- <i class="fas fa-check"></i> -->
+        <i class="fas fa-fingerprint"></i>
+      </span>
+      <span class="business-filter-item__title">
+        {{ item.title }}
+      </span>
+    </div>
   `,
+
+  props: {
+    item: { type: Object, default: () => { } },
+    active: { type: Boolean, default: () => false },
+  },
+  emits: ['selected'],
+
+  setup(props, { emit }) {
+    // When the item is selected.
+    const onSelect = (value) => {
+      emit('selected', value);
+    };
+
+    return { onSelect };
+  },
 };
