c050dce82c57b404d54dd52fca7c4fe5b654fae1
Brian Kariuki Kiragu <bkariuki@hotmail.com>
2021-09-29 18:10:44 +0300

    Business Form component.

--- js/components/BusinessForm.vue.js
+++ js/components/BusinessForm.vue.js
@@ -5,7 +5,143 @@ import useFormatting from '../composables/useFormatting.js';
 
 export default {
   template: `
-    <h1>This is the Business Form</h1>
+    <!-- Button trigger modal -->
+    <button
+      type="button"
+      class="business-list-item-edit__trigger btn btn-primary"
+      data-toggle="modal"
+      :data-target="'#business' + order + 'Backdrop'"
+      v-bind="$attrs"
+    >
+      <slot>{{ text.trigger }}</slot>
+    </button>
+
+    <!-- Modal -->
+    <teleport to="#business-modals">
+      <div
+        :id="'business' + order + 'Backdrop'"
+        class="modal fade"
+        data-backdrop="static"
+        data-keyboard="false"
+        tabindex="-1"
+        :aria-labelledby="'business' + order + 'BackdropLabel'"
+        aria-hidden="true"
+      >
+        <form class="modal-dialog" @submit.prevent="onSubmit">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 :id="'business' + order + 'BackdropLabel'" class="modal-title">
+                {{ text.title }}
+              </h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+
+            <!-- Modal body. -->
+            <div class="modal-body">
+              <!-- Title. -->
+              <div class="form-group">
+                <label :for="'business-' + order + '-title'"> Title </label>
+                <input
+                  :id="'business-' + order + '-title'"
+                  v-model="businessForm.title"
+                  type="text"
+                  class="form-control"
+                  placeholder="What is this business called?"
+                  autocomplete="organization"
+                  required
+                />
+              </div>
+
+              <!-- Category. -->
+              <div v-if="categories" class="form-group">
+                <label :for="'business-' + order + '-category'">Category</label>
+                <input
+                  :id="'business-' + order + '-category'"
+                  v-model="businessForm.category"
+                  type="text"
+                  class="form-control"
+                  placeholder="Select a category or add a new one"
+                  :list="'business-' + order + '-categories'"
+                  required
+                />
+                <datalist :id="'business-' + order + '-categories'">
+                  <option
+                    v-for="category of categories"
+                    :key="'category-' + category.param"
+                    :value="category.param"
+                  ></option>
+                </datalist>
+              </div>
+
+              <div class="form-row">
+                <!-- City. -->
+                <div class="form-group col-md-4">
+                  <label :for="'business-' + order + '-city'">City</label>
+                  <input
+                    :id="'business-' + order + '-city'"
+                    v-model="businessForm.city"
+                    type="text"
+                    class="form-control"
+                    placeholder="Where is this business"
+                    autocomplete="address-level2"
+                    required
+                  />
+                </div>
+
+                <!-- Website. -->
+                <div class="form-group col">
+                  <label :for="'business-' + order + '-website'">Website</label>
+                  <input
+                    :id="'business-' + order + '-website'"
+                    v-model="businessForm.website"
+                    type="url"
+                    class="form-control"
+                    placeholder="https://example.com"
+                    autocomplete="url"
+                  />
+                </div>
+              </div>
+
+              <!-- Notes. -->
+              <div class="form-group">
+                <label :for="'business-' + order + '-notes'">Notes</label>
+                <textarea
+                  :id="'business-' + order + '-notes'"
+                  v-model="businessForm.notes"
+                  class="form-control"
+                  rows="3"
+                  placeholder="Any additional information?"
+                ></textarea>
+              </div>
+            </div>
+
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="submit"
+                class="btn btn-primary"
+                :class="{ disabled: isLoading }"
+              >
+                {{ text.submit }}
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </teleport>
   `,
 
   name: 'BusinessForm',
