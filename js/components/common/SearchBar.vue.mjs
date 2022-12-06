06db7c11d57385d7d8c923be2457592ecba3bec3
Brian Kariuki <bkariuki@hotmail.com>
2022-12-06 15:29:02 +0300

    Added debounce to searchbar

--- js/components/common/SearchBar.vue.mjs
+++ js/components/common/SearchBar.vue.mjs
@@ -6,6 +6,26 @@ const SearchBar = {
 
   setup(props, { emit }) {
     /**
+     * Function to debounce (delay) execution of a function.
+     *
+     * @param {fn} callback Callback function to debounce.
+     * @param {number} delay Delay in milliseconds.
+     *
+     * @returns {fn} Debounced function.
+     * @author Brian Kariuki <bkariuki@hotmail.com>
+     */
+    const debounce = (callback, delay = 250) => {
+      let timeout;
+
+      return (...args) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+          callback(...args);
+        }, delay);
+      };
+    };
+
+    /**
      * Handle when a user searches for a product.
      *
      * @param {HTMLInputEvent} e The input event from the search bar.
@@ -13,13 +33,13 @@ const SearchBar = {
      * @returns {void}
      * @author Brian Kariuki <bkariuki@hotmail.com>
      */
-    const handleSearch = (e) => {
+    const handleSearch = debounce((e) => {
       // Get and trim search value from the input.
       const search = e.target.value.trim();
 
       // Emit the event.
       emit('update:search', search);
-    };
+    }, 1000);
 
     return { handleSearch };
   },
